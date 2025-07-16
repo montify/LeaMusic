3d5cd967d94ed5e2bdf510e78fcbd0bb7a2f9d1e
Alexander Ertl <alexanderertl@outlook.com>
2025-07-16 13:46:56 +0200

    Remove unused using directives

--- LeaMusic/src/Services/Interfaces/IGoogleDriveHandler.cs
+++ LeaMusic/src/Services/Interfaces/IGoogleDriveHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeaMusic.src.Services.Interfaces
+﻿namespace LeaMusic.src.Services.Interfaces
 {
     public interface IGoogleDriveHandler : IResourceHandler
     {
